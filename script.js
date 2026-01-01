84c48df124484d12c44dc4fe3dab0b46d70f0508
asad-fleek <asad.mahmood@pendoah.ai>
2026-01-01 23:41:24 +0500

    projectupgraded

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const form = document.querySelector("form");
 const emailInput = document.querySelector('input[type="email"]');
 const message = document.querySelector("span");
-
+const msg = document.getElementById("msg");
 let storedEmails = JSON.parse(localStorage.getItem("emails")) || [];
 
 
 form.addEventListener("submit", function (e) {
     e.preventDefault(); 
     const email = emailInput.value;
-
+    
     
     storedEmails.push(email);
 
     localStorage.setItem("emails", JSON.stringify(storedEmails));
 
-    message.style.display = "block";
+    message.style.display = "";
+    msg.innerHTML = "Thank You for Subcribing!";
 
     emailInput.value = "";
 });
